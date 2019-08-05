899526d86093cd80d0be77bcd4b021941eb20512
Henri Kotkanen <henri.kotkanen@anders.fi>
2019-08-05 11:01:48 +0300

    Add external network to docker compose, specify db user in entrypoint
    - External network definition is needed to get omahelsinki, open-city-profile and tunnistamo to work together while developing locally.
    - If user is not specified, psql defaults to 'postgres' user. However, this user is not present if container is managed with docker-compose which has a different POSTGRES_USER defined.

--- docker/postgres/docker-entrypoint.sh
+++ docker/postgres/docker-entrypoint.sh
@@ -6,7 +6,7 @@ for ext in postgis hstore; do
     # Add the extensions to the default db template so that any
     # new db will have the extensions enabled includin test db.
     echo "Installing $ext extension for template1"
-    psql template1 -c "$create_ext_sql"
+    psql --username "$POSTGRES_USER" template1 -c "$create_ext_sql"
     echo "Installing $ext extension for $POSTGRES_DB database"
     psql --username "$POSTGRES_USER" "$POSTGRES_DB" -c "$create_ext_sql"
 done
